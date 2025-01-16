68606b483a535a8b72bb1d776f60ce68f9847b70
Eric Helton <Eric.Helton@vu.com>
2025-01-16 07:05:39 -0600

    advanced pay update 1

--- payCalculator.py
+++ payCalculator.py
@@ -1,6 +1,23 @@
 def calculatePay():
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hours = input("Enter Hours: ")
+    hours = float(hours)
+
+    rate = input("Enter Rate: ")
+    rate = float(rate)
+
+    if hours > 40:
+        overtime_hours = hours - 40
+        normal_hours = 40
+
+        overtime_pay = overtime_hours * rate * 1.5
+        normal_pay = normal_hours * rate
+
+        gross_pay = normal_pay + overtime_pay
+    else:
+        gross_pay = hours * rate
+
+    print (f"Pay: {gross_pay}")
     
     # end assignment
 
